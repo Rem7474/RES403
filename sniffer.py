b984c070a73bc718ed81d762bac4eb3f064d1a47
RemEx <74838147+Rem7474@users.noreply.github.com>
2024-03-06 17:32:29 +0100

    update decode

--- sniffer.py
+++ sniffer.py
@@ -1,27 +1,19 @@
 #script utilisant une socket brute pour sniffer les paquets
 
 import socket
-
+import struct
 # créer le socket raw
 sniffer = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
 
-# lier le socket à l'interface publique
-ip_host=("192.168.70.134",0)
-sniffer.bind(ip_host)
-
-# nous voulons les en-têtes IP inclus dans le paquet capturé
-sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
-
-#activer le mode promiscuous
-sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
 
 # lecture des paquets
-
 while True:
     # lire un paquet
     raw_buffer = sniffer.recvfrom(65565)
     # analyse des en-têtes IP
     ip_header = raw_buffer[0][0:20]
+    # decode the ip header
+    ip_header = struct.unpack('!BBHHHBBH4s4s', ip_header)
     # affichage ip src et ip dst
     print("IP : ", ip_header[12:16], " -> ", ip_header[16:20])
     # affichage du type de protocole
