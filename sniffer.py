0adc2c67269a2c91a65e675d4ebe8f47ec114a11
RemEx <74838147+Rem7474@users.noreply.github.com>
2024-03-06 17:13:08 +0100

    sniffer

---
+++ sniffer.py
@@ -0,0 +1,13 @@
+#script utilisant une socket brute pour sniffer les paquets
+
+import socket
+
+# le socket en mode promiscuous
+sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
+
+
+# lire un paquet pour afficher ip src et ip dst + type de protocole
+
+while True:
+    print(sniffer.recvfrom(65565))
+    
