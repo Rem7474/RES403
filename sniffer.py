e5e5cea816aa5c1dc1e987d9b8f0724dc06855d7
RemEx <74838147+Rem7474@users.noreply.github.com>
2024-03-06 17:14:02 +0100

    update socket

--- sniffer.py
+++ sniffer.py
@@ -3,11 +3,10 @@
 import socket
 
 # le socket en mode promiscuous
-sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
+sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW)
 
 
 # lire un paquet pour afficher ip src et ip dst + type de protocole
 
 while True:
     print(sniffer.recvfrom(65565))
-    
