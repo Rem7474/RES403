f23581736160f506eeb9df4739a420773903ae30
RemEx <74838147+Rem7474@users.noreply.github.com>
2024-03-06 16:06:13 +0100

    script python client et server

---
+++ client.py
@@ -0,0 +1,12 @@
+#script client utilisant la librairie socket pour communiquer avec le serveur sur le port 2000
+import socket
+
+# Création d'un objet socket
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connexion au serveur
+IP="192.168.243.128"
+socket.connect((IP, 2000))
+
+# Envoi de données
+socket.send(b'Hello, world')
---
+++ server.py
@@ -0,0 +1,23 @@
+#script serveur utilisant le module socket pour récupérer les données du client depuis le port 2000
+import socket
+
+# Création d'un objet socket
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Obtenir le nom de la machine locale
+host = socket.gethostname()
+port = 2000
+
+# Lier à l'adresse et au port
+serversocket.bind((host, port))
+
+# Nombre maximum de connexions en attente
+serversocket.listen(1)
+
+while True:
+    # Établir une connexion
+    clientsocket,addr = serversocket.accept()
+    print(f"Connexion depuis {addr[0]}:{addr[1]}")
+    msg = 'Connexion réussie : message bien reçu !' + "\r\n"
+    clientsocket.send(msg.encode('utf-8'))
+    clientsocket.close()
