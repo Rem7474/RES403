50f92763d93548dc374ee7cc3eaf36a0d8407d7f
RemEx <74838147+Rem7474@users.noreply.github.com>
2024-03-13 14:27:20 +0100

    update dns

--- dns_resol_enregistrements_1.py
+++ dns_resol_enregistrements_1.py
@@ -1,8 +1,14 @@
 import socket as socket
+def get_ips_par_recherche_dns(cible, port=None):
+    try:
+        ip_address = socket.gethostbyname(cible)
+        print("IP address of the host: ", ip_address)
+    except socket.herror:
+        print("No DNS record found")
 #set dns server
-ip_address="10.108.239.251"
+hostname="iut-acy.local"
 try:
-    hostname, aliaslist, ipaddrlist = socket.gethostbyaddr(ip_address)
+    hostname, aliaslist, ipaddrlist = socket.gethostbyname(hostname)
     print(hostname, aliaslist, ipaddrlist)
 except socket.herror:
     print("No DNS record found")
