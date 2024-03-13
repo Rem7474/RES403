c9a8a150690add9c087f130dce8a65e7b71996a0
RemEx <74838147+Rem7474@users.noreply.github.com>
2024-03-13 15:47:07 +0100

    update scanner

--- scan_ports.py
+++ scan_ports.py
@@ -2,29 +2,32 @@
 from scapy.all import sr1,IP,conf,TCP
 
 #script pour effectué un scan sur tout les ports d'une machine
-#on envoie un paquet TCP SYN sur chaque port
-#si on recoit un pajson SYN/ACK, le port est ouvert
-#si on recoit un paquet RST, le port est fermé
-#si on recoit un paquet ICMP, le port est filtré
+def tcp_scan(hote, port):
+    # Envoi du paquet SYN et réception de la réponse
+    paquet_syn = IP(dst=hote) / TCP(dport=port, flags="S")
+    reponse_syn = sr1(paquet_syn, verbose=False, timeout=1)
 
-#on désactive les messages de scapy
-conf.verb=0
-
-ip=input("Entrez l'adresse IP de la machine à scanner : ")
-#on crée un paquet IP
-paquet=IP(dst=ip)
-#on crée un paquet TCP
-tcp=TCP(dport=(1,1024),flags="S")
-#on envoie le paquet
-reponse=sr1(paquet/tcp)
-#on analyse la réponse
-if reponse.haslayer(TCP):
-    port=reponse[TCP].sport
-    if reponse[TCP].flags==18:
-        print("Port ouvert : ",port)
-    elif reponse[TCP].flags==20:
-        print("Port fermé")
+    if reponse_syn is None:
+        return "Port {} : Filtré / Aucune réponse reçue".format(port)
+    elif reponse_syn.haslayer(TCP) and reponse_syn.getlayer(TCP).flags == 0x12:
+        # Envoi du paquet ACK
+        paquet_ack = IP(dst=hote) / TCP(dport=port, flags="A", ack=reponse_syn.getlayer(TCP).seq + 1)
+        sr1(paquet_ack, verbose=False, timeout=1)
+        # Fermeture de la connexion
+        reponse_fin = sr1(IP(dst=hote) / TCP(dport=port, flags="R"), verbose=False)
+        return "Port {} : Ouvert".format(port)
+    elif reponse_syn.haslayer(TCP) and reponse_syn.getlayer(TCP).flags == 0x14:
+        return "Port {} : Fermé".format(port)
     else:
-        print("Port filtré")
-else:
-    print("Pas de réponse")
+        return "Port {} : État inconnu".format(port)
+
+# Hôte cible
+hote = input("Entrez l'adresse IP de la cible : ")
+
+# Désactiver les messages d'avertissement
+conf.verb = 0
+
+# Scanner tous les ports courants
+for port in range(1, 1025):
+    resultat = tcp_scan(hote, port)
+    print(resultat)
