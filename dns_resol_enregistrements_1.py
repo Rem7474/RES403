fc34654d24a63d90f0c5ecef15d45496daa66be2
RemEx <74838147+Rem7474@users.noreply.github.com>
2024-03-13 14:43:43 +0100

    dns resolv fini

--- dns_resol_enregistrements_1.py
+++ dns_resol_enregistrements_1.py
@@ -1,20 +1,18 @@
 import socket as socket
-def get_ips_par_recherche_dns(cible, port=None):
-    try:
-        ip_address = socket.gethostbyname(cible)
-        print("IP address of the host: ", ip_address)
-    except socket.herror:
-        print("No DNS record found")
-#set dns server
-hostname="iut-acy.local"
+print("Résolution IP")
 try:
     hostname, aliaslist, ipaddrlist = socket.gethostbyaddr("10.108.239.251")
     print(hostname, aliaslist, ipaddrlist)
 except socket.herror:
     print("No DNS record found")
 
+
+print("Résolution nom de domaine")
+hostname="iut-acy.local"
 try:
     dns, alias, prout= socket.gethostbyname_ex(hostname)
     print(dns, alias, prout)
+    #affichage de l'ip du server DNS
+    print(f"Adresse du serveur DNS : {socket.gethostbyname(dns)}")
 except socket.herror:
     print("No DNS record found")
