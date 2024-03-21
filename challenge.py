b58ce131dc9f25e2f8fe08d56a91a44ef6d8aa8c
Rem7474 <recuv007@hotmail.fr>
2024-03-21 11:45:11 +0100

    test

--- challenge.py
+++ challenge.py
@@ -7,9 +7,17 @@ import scapy.all as scapy
 file = scapy.rdpcap("capture.pcapng")
 
 #affichage des paquets
+user=""
 for packet in file:
     #recherche de "USER" dans les data des paquets
-    if "USER" in packet.load:
-        print(packet.load)
+    if "USER" in str(packet.load):
+        user=packet.load.decode('utf-8')
+        print(user)
+    if "PASS" in str(packet.load):
+        password=packet.load.decode('utf-8')
+    if user in str(packet.load):
+        print(packet.load.decode('utf-8'))
+        break
+    
 
 
